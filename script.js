359986122d6c81da96ac013b89761f099bc791c3
Blake Fogle <foglebd@gmail.com>
2019-11-16 10:41:40 -0800

    code is almost full

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-var city = "Moscow,Russia"
-var apiKey = "0784d07688511b276c71076385817e27";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    
-    console.log(queryURL);
-    console.log(response);
-
-    var city = response.name
-    var wind = response.wind.speed
-    var humidity = response.main.humidity
-    var temp = response.main.temp
-
-    $('body').prepend('<h1>'+ city + '5 day weather forecast</h1>')
-    $('.city').text('City: ' + city)
-    $('.wind').text('Wind: ' + wind)
-    $('.humidity').text('Humidity: ' + humidity)
-    $('.temp').text('Temperature (in Kelvin): ' + temp)
-
-    });
-
 $(document).ready(function(){
 
-    $('#searchButton').click(function(){
+    function displayWeatherForecast(){
 
-    });
+        var city = $(".searchTerm").val().trim();
+        var apiKey = "0784d07688511b276c71076385817e27";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+            }).then(function(response){
+                
+                console.log(queryURL);
+                console.log(response);
+
+                var city = response.name
+                var wind = response.wind.speed
+                var humidity = response.main.humidity
+                var temp = response.main.temp
+
+                var weatherWrapper = $('<div>');
+                var contentWrapper = $('<div>');
+
+                contentWrapper.append('<h4>' + city + '</h4>');
+                contentWrapper.append('<p>' + wind + '</p>');
+                contentWrapper.append('<p>' + humidity + '</p>');
+                contentWrapper.append('<p>' + temp + '</p>');
+
+                weatherWrapper.append(contentWrapper);
+                $('#weather-card').append(weatherWrapper);
+            });
+    };
+        $('#searchButton').click(function(){
+            displayWeatherForecast();
+        });
 });
